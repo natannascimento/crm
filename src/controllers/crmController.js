a143c4ca10dc81a028696f6c7e3891f0fe2fe872
natannascimento <natan.graves@gmail.com>
2023-08-31 23:43:53 -0300

    first commit

---
+++ src/controllers/crmController.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import {ContactSchema} from '../models/crmModel';
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+export const addNewContact = async (req, res) => {
+    let newContact = new Contact(req.body);
+    let contact = await 
+    newContact.save();
+    res.json(contact)
+};
+
+export const getContacts = async (req, res) => {
+    let contacts = await Contact.find({});
+    res.json(contacts);
+};
